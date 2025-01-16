fa9690f19f24b2dae56079d1e8304bdb4844a913
Mohaseen-A <mohaseen.tricksfree@gmail.com>
2025-01-16 15:02:13 +0530

    alignment-change

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -20,11 +20,11 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/mohaseen1998/">linkedin.com/Mohaseen A</a>
+          <a href="https://www.linkedin.com/in/mohaseen1998/" target="blank">linkedin.com/Mohaseen A</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://github.com/Mohaseen-A">github.com/Mohaseen A</a>
+          <a href="https://github.com/Mohaseen-A" target="blank">github.com/Mohaseen A</a>
         </li>
       </ul>
     </footer>
